23745f81c5eef5b30a6058f3cde4d17ff5e17199
Daniel Diaz <daniel.diaz@univ-paris1.fr>
2000-12-19 14:41:35 +0000

    new Prolog examples (+count) + foreign examples

--- examples/ExamplesC/new_main_c.c
+++ examples/ExamplesC/new_main_c.c
@@ -22,8 +22,6 @@
  * 59 Temple Place - Suite 330, Boston, MA 02111, USA.                     *
  *-------------------------------------------------------------------------*/
 
-/* $Id$ */
-
 #include <string.h>
 
 #include "gprolog.h"
