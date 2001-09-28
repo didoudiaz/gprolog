f972241748eea83c07a11c1c5a728e4894a39016
Daniel Diaz <daniel.diaz@univ-paris1.fr>
2001-09-28 18:30:21 +0000

    starting 1.2.8

--- src/EnginePl/gprolog.h
+++ src/EnginePl/gprolog.h
@@ -29,13 +29,14 @@
 #define HAVE_MALLOC_H 1
 #define HAVE_MMAP 1
 #define HAVE_MALLOPT 1
+#define HAVE_MKSTEMP 1
 #define NO_USE_EBP 1
 #define NO_USE_GUI_CONSOLE 1
 #define M_ix86 1
 #define M_linux 1
 #define M_ix86_linux 1
 #define PROLOG_NAME1               "gprolog"
-#define PROLOG_NAME                "GNU Prolog"
+#define PROLOG_NAME                "Contexts>>GNU Prolog"
 #define PROLOG_VERSION             "1.2.8"
 #define PROLOG_DATE                "Sep 28 2001"
 #define PROLOG_COPYRIGHT           "Copyright (C) 1999-2001 Daniel Diaz"
