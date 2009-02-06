cc77b585c2b10ad2c80e2ca26f8deaea0150b103
Daniel Diaz <daniel.diaz@univ-paris1.fr>
2009-02-06 12:22:18 +0000

    add sigaction detection

--- src/EnginePl/try_sigaction.c
+++ src/EnginePl/try_sigaction.c
@@ -1,3 +1,29 @@
+/*-------------------------------------------------------------------------*
+ * GNU Prolog                                                              *
+ *                                                                         *
+ * Part  : configuration                                                   *
+ * File  : pl_config.c                                                     *
+ * Descr.: C Compiler options and WAM Configuration                        *
+ * Author: Daniel Diaz                                                     *
+ *                                                                         *
+ * Copyright (C) 1999-2009 Daniel Diaz                                     *
+ *                                                                         *
+ * GNU Prolog is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2, or any later version.       *
+ *                                                                         *
+ * GNU Prolog is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU        *
+ * General Public License for more details.                                *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc.  *
+ * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.               *
+ *-------------------------------------------------------------------------*/
+
+/* $Id$ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
